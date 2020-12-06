3204d38c83ce20a7fca06b9f122e7826dc09ead2
Zach <zacharycoss@gmail.com>
2020-12-06 11:01:02 -0600

    updating statuscheck

--- src/services/tools/jobs.js
+++ src/services/tools/jobs.js
@@ -1,19 +1,38 @@
 const 
     cron = require('cron'),
     CronJob = cron.CronJob,
-    usage = require('./usage');
+    usage = require('./usage'),
+    statuscheck = require('./statuscheck');
 
 module.exports.init = () => {
     console.log('initializing billing jobs')
 
-    // usage.calculateUsage()
-
-    // // Runs every minute
+    // Calculate usage (every minute)
     const job = new CronJob('0 */1 * * * *', function() {
         usage.calculateUsage()
     }, null, true)
 
-    // // Initialize job
+    // Status check (15 second interval)
+    const statuscheck15 = new CronJob('*/15 * * * * *', function() {
+        statuscheck.startStatuschecks(15)
+    }, null, true)
+
+    // Status check (30 second interval)
+    const statuscheck30 = new CronJob('*/30 * * * * *', function() {
+        statuscheck.startStatuschecks(30)
+    }, null, true)
+
+    // Status check (60 second interval)
+    const statuscheck60 = new CronJob('*/60 * * * * *', function() {
+        statuscheck.startStatuschecks(60)
+    }, null, true)
+
+
+    // // Initialize jobs
     job.start()
+    statuscheck15.start()
+    statuscheck30.start()
+    statuscheck60.start()
+
     
 }
---
+++ src/services/tools/statuscheck.js
@@ -0,0 +1,42 @@
+const 
+    _ = require('lodash'),
+    IndexSchema = require('./schema').schema,
+    Axios = require('axios'),
+    Agent = require('agentkeepalive'),
+    keepAliveAgent = new Agent({
+        maxSockets: 100,
+        maxFreeSockets: 10,
+        timeout: 60000, // active socket keepalive for 60 seconds
+        freeSocketTimeout: 30000, // free socket keepalive for 30 seconds
+    }),
+    axios = Axios.create({httpAgent: keepAliveAgent});
+
+module.exports = {
+    requestStatusCheck: async function(statuscheck) {
+        const request = await axios({
+            url: `${process.env.API_URL}/statuscheck-workflow/${statuscheck.workflowId}`,
+            method: 'POST',
+        })
+        return request
+    },
+    startStatuschecks: async function(interval) {
+        console.log(`Searching for statuschecks at ${interval} seconds`)
+
+        const statuschecks = await IndexSchema.Statuscheck.find({ interval: interval, active :true, status: 'running', })
+
+        console.log(`Found ${_.size(statuschecks)} statuschecks at ${interval} seconds`)
+
+        for (const statuscheck of statuschecks) {
+            try {
+                console.log('requesting statuscheck: ', statuscheck._id)
+                await module.exports.requestStatusCheck(statuscheck)
+            } catch(err) {
+                if (err.response && err.response.data) {
+                    console.log('statuscheck request error', err.response.data)
+                } else {
+                    console.log('statuscheck request error', err.message)
+                }
+            } 
+        }
+    },
+}
