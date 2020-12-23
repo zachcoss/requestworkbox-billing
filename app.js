a9bd002050583c633cbd29e34a70d3d0e202c49a
Zach <zacharycoss@gmail.com>
2020-12-23 12:30:19 -0600

    updating logging

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const compression = require('compression')
 const jobs = require('./src/services/tools/jobs');
 
 const http = require('http');
@@ -22,9 +23,8 @@ app.set('port', port);
 app.set('x-powered-by', false)
 app.set('json escape', true)
 
-const compression = require('compression')
+app.use(logger('combined'));
 app.use(compression())
-app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
